28fe031c38c2cd56c7785e688d5f0962c13544a5
leandroluizpereira <leandro.2306@hotmail.com>
2021-03-05 12:38:25 -0300

    Exércicio-01

---
+++ básico/intermediário/exércicio-01.py
@@ -0,0 +1,64 @@
+print(' 1) Implemente um programa que escreve na tela a seguinte frase "Meu primeiro programa em Python!!!.')
+
+print('\n')
+
+print('Solução:    ')
+
+print('Meu primeiro programa em Python !!!')
+
+print('\n')
+
+print('2) Você foi contratado por uma escola para fazer o sistema de boletim dos alunos.')
+print('Como primeiro passo, escreva um programa que produza a seguinte caixa (Não é necessária manter o alinhamento)')
+
+print('\n')
+
+print('Solução:  ')
+print('Aluno(a)   Nota')
+print('Ana Maria    9.0 ')
+print('José Carlos  7.20 ')
+print('Júlio Martins    8.0 ')
+print('libiratan Santos  10.00')
+
+print('\n')
+
+print('3) Utilizando variáveis,faça um programa para realizar o seguinte cálculo:')
+print(' 10 + 20 - 30 +  (45*3)/ 8 **2 ')
+
+print('\n')
+
+print('Solução: ')
+float = sum = (10+20-30)
+float = sum2 = ((45*3)//8**2)
+total=(sum + sum2)
+print('%0.4f'%total)
+
+print('\n')
+ 
+print ('4) Faça um programa qu leia duas variavéis no teclado e apresent as duas soma entre eles')
+
+print('\n')
+
+print('Solução:')
+n1=int(input('Digíte o primeiro número: '))
+n2=int(input('Digíte o segundo número: '))
+soma = (n1 + n2)
+print('A soma de ',n1,'e de ',n2,' e:',soma)
+
+
+print('\n')
+
+print('5) Calcule o delta da equação a seguir, realizando a entrada dos valores de (a,b,c) pelo teclado \n')
+print('Exemplo: x² + 12x - 13 = (a =1,b=12 e c=13)')
+
+print('\n')
+
+print('Solução:')
+a=int(input('Digite o valor a '))
+b=int(input('Digite o valor b '))
+c=int(input('Digite o valor c '))
+
+soma = (pow(a,2)+b-c)
+
+print(soma)
+
